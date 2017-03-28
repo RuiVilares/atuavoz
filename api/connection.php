7b1acec69386ed974af9e932ba3e4a95e2f80eac
Rui Vilares <rui.vilares@hotmail.com>
2017-03-28 11:49:38 +0100

    new description and bd fixed

--- api/connection.php
+++ api/connection.php
@@ -1,6 +1,13 @@
 <?php
+
+  $username = 'sql11166133';
+  $password = 'IJeXyTYeJY';
+  $host = 'sql11.freemysqlhosting.net';
+  $dbname = 'sql11166133';
+  $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
   try {
-      $db = new PDO('sqlite:./database/mysql.db');
+      $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
+      //$db = new PDO('sqlite:./database/mysql.db');
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $ex) {
       die("Failed to connect to the database: ".$ex->getMessage());
