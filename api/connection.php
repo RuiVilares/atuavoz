1d9e6cab9e500fc0d2a70650a5d5e237fdfe4474
Rui Vilares <rui.vilares@hotmail.com>
2017-05-21 23:32:03 +0100

    Update db connection info

--- api/connection.php
+++ api/connection.php
@@ -1,10 +1,11 @@
 <?php
 
-  $username = 'sql11166133';
-  $password = 'IJeXyTYeJY';
-  $host = 'sql11.freemysqlhosting.net';
-  $dbname = 'sql11166133';
+  $username = 'b8c1e5be7ccd6d';
+  $password = 'a63ee240';
+  $host = 'eu-cdbr-west-01.cleardb.com';
+  $dbname = 'heroku_0e318643a0823d9';
   $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+  
   try {
       $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
