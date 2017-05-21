04b356255a9d5c080606fa27305f66692be226bc
Rui Vilares <rui.vilares@hotmail.com>
2017-05-21 22:01:09 +0100

    delete connection line

--- api/connection.php
+++ api/connection.php
@@ -7,7 +7,6 @@
   $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
   try {
       $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
-      //$db = new PDO('sqlite:./database/mysql.db');
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $ex) {
       die("Failed to connect to the database: ".$ex->getMessage());
